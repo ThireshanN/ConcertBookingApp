e1b0154b6099c64aaccac764e98d1ebf02c589c5
ThireshanN <thireshannaidoo@gmail.com>
2023-04-30 00:06:32 +1200

    adding untested getConcertSummaries

--- concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
@@ -1,19 +1,51 @@
 package proj.concert.service.services;
 
+import proj.concert.common.dto.ConcertSummaryDTO;
+import proj.concert.service.domain.Concert;
+import proj.concert.service.mapper.ConcertMapper;
 
+import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Path("/concert-service")
-@Produces({
-        javax.ws.rs.core.MediaType.APPLICATION_JSON
-})
-@Consumes({
-        javax.ws.rs.core.MediaType.APPLICATION_JSON
-})
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ConcertResource {
 
-    // TODO Implement this.
+    private EntityManager em = PersistenceManager.instance().createEntityManager();
+
+    @GET
+    @Path("/concert-summaries")
+    public Response getConcertSummaries() {
+        try {
+            List<ConcertSummaryDTO> summaries = em.createQuery("SELECT c FROM Concert c", Concert.class)
+                    .setLockMode(LockModeType.PESSIMISTIC_READ)
+                    .getResultList().stream()
+                    .map(ConcertMapper::toSummaryDTO)
+                    .collect(Collectors.toList());
+
+            if (summaries.isEmpty()) {
+                return Response.noContent().build();
+            }
+
+            GenericEntity<List<ConcertSummaryDTO>> entity = new GenericEntity<List<ConcertSummaryDTO>>(summaries) {};
+            return Response.ok(entity).build();
+        } finally {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            em.close();
+        }
+    }
 
 }
