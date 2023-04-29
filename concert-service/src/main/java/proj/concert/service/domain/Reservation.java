0cfa1a9aa1caac39580213030e38bf68a4913d00
josie <jche530@aucklanduni.ac.nz>
2023-04-29 16:20:13 +1200

    Add Reservation class in domain constructors, getters and setters, added with Reservation class

---
+++ concert-service/src/main/java/proj/concert/service/domain/Reservation.java
@@ -0,0 +1,77 @@
+package proj.concert.service.domain;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+
+@Entity
+@Table(name = "RESERVATIONS")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long concertId;
+
+    private LocalDateTime concertDate;
+
+    @OneToOne
+    @JoinColumn(name = "seat_id")
+    private Seat seat;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Constructors, getters, setters, and other methods
+
+    public Reservation() {
+    }
+
+    public Reservation(User user, long concertId, LocalDateTime date) {
+        this.user = user;
+        this.concertId = concertId;
+        this.concertDate = concertDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public long getConcertId() {
+        return concertId;
+    }
+
+    public void setConcertId(long concertId) {
+        this.concertId = concertId;
+    }
+
+    public LocalDateTime getDate() {
+        return concertDate;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.concertDate = date;
+    }
+
+}
