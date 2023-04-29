09c412689ec8d6f298de8e89d9731c75a9bbec70
josie <jche530@aucklanduni.ac.nz>
2023-04-29 16:09:18 +1200

    Add Performer class in domain -constructors, getters and setters, added with Performer class

--- concert-service/src/main/java/proj/concert/service/domain/Performer.java
+++ concert-service/src/main/java/proj/concert/service/domain/Performer.java
@@ -1,2 +1,46 @@
-package proj.concert.service.domain;public class Performer {
+package proj.concert.service.domain;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+
+
+@Entity
+@Table(name = "performers")
+public class Performer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "performers")
+    private Set<Concert> concerts = new HashSet<>();
+
+    public Performer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
