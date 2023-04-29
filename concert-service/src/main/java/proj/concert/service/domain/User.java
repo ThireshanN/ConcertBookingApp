467f26dbac94cab4b4c18ff0e085536674c543c5
josie <jche530@aucklanduni.ac.nz>
2023-04-29 16:20:58 +1200

    Add User class in domain constructors, getters and setters, added with User class

--- concert-service/src/main/java/proj/concert/service/domain/User.java
+++ concert-service/src/main/java/proj/concert/service/domain/User.java
@@ -1,2 +1,58 @@
-package proj.concert.service.domain;public class User {
+package proj.concert.service.domain;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Reservation> reservations = new HashSet<>();
+
+    // Constructors, getters, setters, and other methods
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(Set<Reservation> bookings) {
+        this.reservations = reservations;
+    }
 }
