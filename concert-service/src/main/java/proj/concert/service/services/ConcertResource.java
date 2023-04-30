e75b16867972479ba895e7d741ba3c4ceda0c6f8
josie <jche530@aucklanduni.ac.nz>
2023-04-30 22:01:49 +1200

    getAllConcerts implementation complete The implementation is passing the ConcertResouceIT test

--- concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
+++ concert-service/src/main/java/proj/concert/service/services/ConcertResource.java
@@ -1,6 +1,7 @@
 package proj.concert.service.services;
 
 import proj.concert.common.dto.ConcertSummaryDTO;
+import proj.concert.common.dto.ConcertDTO;
 import proj.concert.service.domain.Concert;
 import proj.concert.service.mapper.ConcertMapper;
 
@@ -13,14 +14,48 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.log4j.Logger;
 
 @Path("/concert-service")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ConcertResource {
+
+    private static final Logger LOGGER = Logger.getLogger(ConcertResource.class);
+    @GET
+    @Path("/concerts")
+    public Response getAllConcerts() {
+        EntityManager em = PersistenceManager.instance().createEntityManager();
+        try {
+            em.getTransaction().begin();
+            List<Concert> concerts = em.createQuery("SELECT c FROM Concert c", Concert.class)
+                    .getResultList();
+
+            if (concerts.isEmpty()) {
+                return Response.noContent().build();
+            }
+
+            List<ConcertDTO> concertDTOS = new ArrayList<>();
+            for (Concert concert : concerts) {
+                concertDTOS.add(ConcertMapper.toDTO(concert));
+            }
+            em.getTransaction().commit();
+            GenericEntity<List<ConcertDTO>> out = new GenericEntity<List<ConcertDTO>>(concertDTOS) {};
+            return Response.ok(out).build();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+//            e.printStackTrace(); //FOR DEBUGGING
+            return Response.serverError().entity("Error retrieving concerts: " + e.getMessage()).build();
+        } finally {
+            em.close();
+        }
+    }
 
     private EntityManager em = PersistenceManager.instance().createEntityManager();
 
